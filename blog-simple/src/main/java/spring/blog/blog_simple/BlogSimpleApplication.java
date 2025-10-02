ec4c122e54650abb2792ad1354bbd1436acad998
Edgar Alejandro Alcocer Reyes <aspire5@outlook.es>
2025-10-02 14:04:32 -0600

    First commit

---
+++ blog-simple/src/main/java/spring/blog/blog_simple/BlogSimpleApplication.java
@@ -0,0 +1,13 @@
+package spring.blog.blog_simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogSimpleApplication.class, args);
+	}
+
+}
---
+++ blog-simple/src/test/java/spring/blog/blog_simple/BlogSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.blog.blog_simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
